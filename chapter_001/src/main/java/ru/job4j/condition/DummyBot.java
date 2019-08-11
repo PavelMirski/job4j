6bf10cb85f4c8c96b7c4055ac62758b100139585
PavelMirski <pavel.mirski@tptlive.ee>
2019-08-12 00:45:56 +0300

    DummyBot_doesn't work

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,4 +1,4 @@
-package ru.job4j.firstbot;
+package ru.job4j.condition;
 
 /**
  * @author Petr Arsentev (parsentev@yandex.ru)
@@ -8,17 +8,21 @@ package ru.job4j.firstbot;
 public class DummyBot {
     /**
      * Отвечает на вопросы.
+     *
      * @param question Вопрос от клиента.
      * @return Ответ.
      */
+//public static void main(String[] args)
+
+
     public String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             // заменить ... на правильный ответ rsl = "ответ по заданию".
-            rsl = ...;
-        } else if (....) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
             // заменить ... на правильный ответ rsl = "ответ по заданию".
-            rsl = ..;
+            rsl = "До скорой встречи.";
         }
         return rsl;
     }
--- chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.firstbot;
+package ru.job4j.condition;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
