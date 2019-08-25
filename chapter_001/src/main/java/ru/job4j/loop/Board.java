789b3f06541fcf2ebd5eb468aa4a066ba81f1122
Pavel Mirski <pavel.mirski@gmail.com>
2019-08-25 22:50:36 +0300

    Board started

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,20 @@
+public class Board {
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for ( ) {
+            for ( ) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if () {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
