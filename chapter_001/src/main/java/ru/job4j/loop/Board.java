0561bfa0885c731d031ff2ab07c84eaf3ecce693
PavelMirski <pavel.mirski@tptlive.ee>
2019-08-27 23:40:41 +0300

    added Board

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,20 +1,25 @@
+package ru.job4j.loop;
+
 public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for ( ) {
-            for ( ) {
+        for (int i = 1;i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if () {
-                    screen.append("X");
+                if ((j + i) % 2 == 0) {
+                    screen.append("x");
                 } else {
                     screen.append(" ");
                 }
             }
             // добавляем перевод на новую строку.
             screen.append(ln);
+
         }
+
         return screen.toString();
+
     }
 }
