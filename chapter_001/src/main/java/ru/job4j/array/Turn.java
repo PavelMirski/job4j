e0b09408eb90f050bae98cd4b1ee7b10aca760fa
Pavel Mirski <pavel.mirski@gmail.com>
2019-09-13 23:19:18 +0300

    added Turn

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array) {
+        int n = array.length;
+        int t;
+        for (int i=0; i < n/2; i++) {
+            t = array[n - i - 1];
+            array[n - i - 1] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
+}
