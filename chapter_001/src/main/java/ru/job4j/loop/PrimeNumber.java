130920dae84fb5f24e5cc24f2f68a07fbb4401f4
PavelMirski <pavel.mirski@tptlive.ee>
2019-09-20 20:46:53 +0300

    commit from win10

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,15 +3,17 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        nextPrime:
+        //nextPrime:
+        int nextPrime = 0;
         for (int i = 2; i <= finish; i++) {
 
             for (int j = 2; j < i; j++) {
-                if ((i % j) == 0) {
-                    continue nextPrime;
-                }
+                if ((i % j) != 0) {
+                    nextPrime ++; ;
+
+                }//nextPrime ++;
             }
-            count++;
+            count = nextPrime;
         }
 
 
