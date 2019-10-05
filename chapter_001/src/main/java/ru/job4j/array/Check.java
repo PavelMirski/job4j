13b0bf0e1ef87f6253d1519a1e9b1ede23b3b428
Pavel Mirski <pavel.mirski@gmail.com>
2019-10-05 21:36:45 +0300

    Check fixed

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,7 +3,16 @@ package ru.job4j.array;
 public class Check {
     public boolean mono(boolean[] data) {
         boolean result = false;
-        int a = 0 ;
+        int i;
+        for (i = 0; i < data.length; i++) {
+            if (data[0] != data[i]) {
+                result = false;
+                break;
+            }
+            result = true;
+        }
+
+       /* int a = 0;
         int b = 0;
         for (int z = 0; z < data.length; z++){
             a += ((data[z] == true) ? 0 : 1 );
@@ -14,10 +23,12 @@ public class Check {
                 result = true;
             else if (a > 0 && b > 0)
                 result = false;
+        }*/
 
+
+            return result;
         }
 
 
-        return result;
-    }
+
 }
