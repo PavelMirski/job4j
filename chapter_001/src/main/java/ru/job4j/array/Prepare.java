087e6d9add6bc8e136b51d7f1c434b0ff88fcfee
Pavel Mirski <pavel.mirski@gmail.com>
2019-10-06 02:02:07 +0300

    ArrayChar added

--- chapter_001/src/main/java/ru/job4j/array/Prepare.java
+++ chapter_001/src/main/java/ru/job4j/array/Prepare.java
@@ -1,25 +1,7 @@
+/*
 package ru.job4j.array;
 
 public class Prepare {
     public static void main(String[] args) {
-          boolean[] data = {true, true, true, true};
-        boolean result = false;
-        int a = 0 ;
-        int b = 0;
-        for (int z = 0; z < data.length; z++){
-            a += ((data[z] == true) ? 0 : 1 );
 
-            b += ((data[z] == false) ? 1 : 0 );
-
-            if(a == 0 || b== 0)
-                result = true;
-            else
-                break;
-        }
-
-            System.out.println(a);
-            System.out.println(b);
-        }
-    }
-
-
+}*/
