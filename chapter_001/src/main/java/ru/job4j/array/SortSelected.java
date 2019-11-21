5cd6ccbed3446168c3d950b985908d715f73f12e
Pavel Mirski <pavel.mirski@gmail.com>
2019-11-21 20:53:37 +0200

    SortSelectedFixed

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -4,11 +4,18 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        int min = MinDiapason.findMin(data, 0, data.length);
-        int index = FindLoop.indexOf(data, min, 0, data.length);
-        for (int i = 0; i < data.length; i++){
+        for (int i = 0; i < data.length-1; i++) {
+            int min = MinDiapason.findMin(data, i + 1, data.length);
+            int index = FindLoop.indexOf(data, min, i + 1, data.length);
+            if (data[i] > min) {
+                int tmp = data[i];
+                data[i] = data[index];
+                data[index] = tmp;
+            }
+        }
+        /*for (int i = 0; i < data.length; i++){
             min = data[i];
-            index = i;
+            //index = i;
             for (int j = i+1; j < data.length; j++){
                 if (data[j] < min){
                     index = j;
@@ -19,7 +26,7 @@ public class SortSelected {
                 data[index] = data[i];
                 data[i] = min;
             }
-        }
+        }*/
 
 
         return data;
