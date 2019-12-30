bd4acce0d91b7e6ae2c88bc9c41b0ec07533e7b9
Pavel Mirski <pavel.mirski@gmail.com>
2019-12-30 07:18:30 +0200

    refactoring_Point

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,5 +1,35 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+}
+
+
+/*
+package ru.job4j.condition;
+
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
         double a = Math.pow(x2 - x1, 2);
@@ -18,4 +48,4 @@ public class Point {
         double third = distance(-2, -7, 3, 9);
         System.out.println("third (-2, -7) to (3, 9) " + third);
     }
-}
+}*/
