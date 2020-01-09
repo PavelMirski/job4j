e8e095b4ba10009bf2305c7643764d7b834f48bc
Pavel Mirski <pavel.mirski@gmail.com>
2020-01-09 06:28:57 +0200

    max_refactoring

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,12 +3,20 @@ package ru.job4j.condition;
 public class Max {
 
 
-    public int max(int left, int right) {
-        //left = 3;
-        //right = 2;
+    public int max(int first, int second) {
+        int result = first > second ? first : second;
+        return result;
+    }
 
-        int result = left > right ? left : right;
+    public int max(int first, int second, int third) {
+        int tmp = max(first, second);
+        int result = third > tmp ? third : tmp;
+        return result;
+    }
 
+    public int max(int first, int second, int third, int fourth) {
+        int tmp = max(first, second, third);
+        int result = fourth > tmp ? fourth : tmp;
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -11,4 +11,18 @@ public class MaxTest {
         int result = max.max(1, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenMax1To3Then3() {
+        Max max = new Max();
+        int result = max.max(1, 2,3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax1To4Then4() {
+        Max max = new Max();
+        int result = max.max(1, 2,3, 4);
+        assertThat(result, is(4));
+    }
 }
