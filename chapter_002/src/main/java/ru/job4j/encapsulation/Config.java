71272d8933f984c91da21970fdc19824db7489d5
Pavel Mirski <pavel.mirski@gmail.com>
2020-01-27 06:43:13 +0200

    Encapsulation

--- chapter_002/src/main/java/ru/job4j/encapsulation/Config.java
+++ chapter_002/src/main/java/ru/job4j/encapsulation/Config.java
@@ -21,3 +21,28 @@ public class Config {
         return key;
     }
 }
+
+/*
+package ru.job4j.encapsulation;
+
+public class Config {
+    String name;
+    protected int position;
+    public String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+    void print() {
+        System.out.println(position);
+    }
+
+    protected String getProperty(String key) {
+        return search(key);
+    }
+
+    private String search(String key) {
+        return key;
+    }
+}*/
