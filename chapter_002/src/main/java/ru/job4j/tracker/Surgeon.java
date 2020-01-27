cc1653555965c87f430fb68329bca3d2ee47d992
Pavel Mirski <pavel.mirski@gmail.com>
2020-01-27 06:40:17 +0200

    manynew

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Surgeon.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+public class Surgeon extends Doctor {
+    private int removeHands;
+    private int installNewHeard;
+
+    public void checkSuccsess() {
+
+    }
+
+
+
+}
